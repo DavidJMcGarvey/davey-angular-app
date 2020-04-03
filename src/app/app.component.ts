40f562b605930b0149a4ea1ee6e95ef9be27ef02
David J McGarvey <davidjmcgarvey@gmail.com>
2020-04-03 11:48:01 -0700

    seperate template and styles into files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h2>Hello World!</h2>',
-  styles: [
-    `
-    h2 {
-      font-family: sans-serif;
-      font-soze: 1.2em;
-    }
-    `
-  ]
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.css"]
 })
 export class AppComponent {
 
