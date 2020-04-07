1c2f30da5472498da9845c6ebe4130953aae04a6
David J McGarvey <davidjmcgarvey@gmail.com>
2020-04-07 12:05:09 -0700

    add form validation

--- src/app/entries/shared/entry.service.ts
+++ src/app/entries/shared/entry.service.ts
@@ -9,6 +9,11 @@ export class EntryService {
 
   }
 
+  addComment(entryId: number, comment: { name: string; comment: string;}) {
+    return this.http.post(`/app/entries/${entryId}/comments`, comment)
+            .toPromise();
+  }
+
   getEntries(): Promise<Entry[]> {
     return this.http.get('/app/entries')
             .toPromise()
